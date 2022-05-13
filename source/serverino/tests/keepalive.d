1c70bbbb082966ef647c4faf970b714ca87d7693
Andrea Fontana <trikkuz@gmail.com>
2022-05-13 18:40:16 +0200

    Checking if connection is reused by curl on keep-alive test

--- source/serverino/tests/keepalive.d
+++ source/serverino/tests/keepalive.d
@@ -30,12 +30,43 @@ unittest
     string headers;
     string content;
 
-    auto http = HTTP("http://localhost:8080/");
+    import core.sys.posix.unistd : STDERR_FILENO, dup, dup2;
+    import core.sys.posix.fcntl;
 
+    import std.process : pipe;
+
+    auto http = HTTP("http://localhost:8080/");
     http.onReceiveHeader = (in char[] key, in char[] value) { headers ~= key~":"~value~"\n"; };
     http.onReceive = (ubyte[] data) { content ~= cast(const char[]) data; return data.length; };
+    http.verbose(true);
+
+    stderr.flush();
+
+    auto debugPipe = pipe();
+    int stderrCopy = dup(STDERR_FILENO);
+    dup2(debugPipe.writeEnd.fileno, STDERR_FILENO);
+    int flags = fcntl(debugPipe.readEnd.fileno, F_GETFL, 0);
+    fcntl(debugPipe.readEnd.fileno, F_SETFL, flags | O_NONBLOCK);
+
+    http.perform();
+    assert(content == "HELLO");
+    content = string.init;
     http.perform();
 
+    dup2(stderrCopy, STDERR_FILENO);
+
+    bool reusingConnection = false;
+    foreach(l; debugPipe.readEnd.byLine())
+    {
+        if (l.empty) break;
+        if (l.canFind("Re-using existing connection"))
+        {
+            reusingConnection = true;
+            break;
+        }
+    }
+
+    assert(reusingConnection == true);
     assert(content == "HELLO");
     assert(headers.toLower.canFind("connection:keep-alive\n"));
 }
