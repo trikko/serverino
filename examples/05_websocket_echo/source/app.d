2e79580abae96227792d8329b729fb228c23ec68
Andrea Fontana <trikkuz@gmail.com>
2024-04-02 01:19:33 +0200

    websocket example

---
+++ examples/05_websocket_echo/source/app.d
@@ -0,0 +1,35 @@
+module app;
+
+import std;
+
+// Docs: https://trikko.github.io/serverino/
+// Tips and tricks: https://github.com/trikko/serverino/wiki/
+// Examples: https://github.com/trikko/serverino/tree/master/examples
+import serverino;
+
+mixin ServerinoMain;
+
+// Accept a new connection only if the request URI is "/echo"
+@onWebSocketUpgrade bool onUpgrade(Request req) {
+	return req.uri == "/echo";
+}
+
+// Handle the WebSocket connection
+@endpoint void echo(Request r, WebSocketProxy ws) {
+
+	// Read messages from the client
+	while (true) {
+
+		// Only if the message is valid
+		if (WebSocketMessage msg = ws.receiveMessage())
+		{
+			// Send the message back to the client
+			ws.sendText("I received your message: `" ~ msg.asString ~ "`");
+		}
+	}
+}
+
+@route!"/"
+@endpoint void index(Request r, Output o) {
+	o ~= readText("html/index.html");
+}
